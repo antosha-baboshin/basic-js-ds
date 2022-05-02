9d894a36ce66a062f1e9a8d2d99c63719bc333f4
Antosha Baboshin <antosha.js@gmail.com>
2022-05-02 15:12:38 +0300

    feat: end basic-js-ds

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Stack {
+  constructor() {
+  this.array = [1, 2];
+  }
+  
+  push(el) {
+    this.array.push(el);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array[this.array.length - 1];
   }
 }
 
